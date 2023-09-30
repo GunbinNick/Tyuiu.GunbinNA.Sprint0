56f2f82b49f6a68586427ad80487fd4aeb9a039f
Tyuiu.GunbinNA@outlook.com <gunbin1005@gmail.com>
2023-09-30 20:14:29 +0500

    Task1

---
+++ Tyuiu.GunbinNA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GunbinNA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Олег";
+            int age = 41;
+            Console.WriteLine("Салам, брат, меня звать " + name + " " + age + " год от роду, б*я буду");
+            Console.ReadKey();
+        }
+    }
+}
